29876ca99a2bb11807bad4314143d9b646e95342
Thulio Carvalho <thuliofmcarvalho@gmail.com>
2019-12-19 11:54:13 -0300

    Mais codigos adicionados em NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,9 +2,20 @@
 //O controller captura as ações do usuário
 class NegociacaoController {
 
+    //Buscando os elementos do DOM(HTML) e guardando nas propriedades da classe
+    constructor() {
+
+        //bind(document) = está associando o document ao querySelector 
+        let $ =  document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
+
     //Criando o método adiciona passando o evento
     adiciona(evento) {
         event.preventDefault(); //Cancelando o comportamento padrão pra que a página não recaregue
-        alert("Chamei ação no controller")
+
+        
     }
 }
